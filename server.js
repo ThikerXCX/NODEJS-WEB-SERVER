449510509dfa42a764fc2a2cbd04486772b6a9a5
ThikerXCX <rifkimuhammad967@gmail.com>
2022-06-23 17:08:34 +0700

    make new server with hapi

--- server.js
+++ server.js
@@ -1,46 +1,13 @@
-const http = require('http');
+const Hapi = require('@hapi/hapi');
 
-/**
- * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
- * 
- * @param request: objek yang berisikan informasi terkait permintaan
- * @param response: objek yang digunakan untuk menanggapi permintaan
- */
-const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
-
-    const {method,url} = request;
-/*
-    if(method === 'GET') {
-        response.end('<h1>Hello!</h1>');
-    }
-    if(method === 'POST') {
-        let body = [];
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        });
-    }
-    if(method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
-    if(method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
-    }
-*/
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
-
-const server = http.createServer(requestListener);
-
-const port = 5000;
-const host = 'localhost';
-
-server.listen(port,host,()=>{
-    console.log("yeay");
-});
+ 
+init();
